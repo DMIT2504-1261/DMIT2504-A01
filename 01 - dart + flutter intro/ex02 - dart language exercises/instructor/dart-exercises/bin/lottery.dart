ff9e160c30dd446f779fc7c234918afac9fcdcae
Oliver Antoniu <oliver+github@abolish.golf>
2026-09-10 10:56:05 -0600

    add [01 - intro]: ex02 - ARRAYS: verbal plan for lottery example

---
+++ 01 - dart + flutter intro/ex02 - dart language exercises/instructor/dart-exercises/bin/lottery.dart
@@ -0,0 +1,7 @@
+// inputs?  - two user inputs: number count <int>, upper limit (inclusive) <int>
+// outputs? - a series of numbers to a terminal (List<int>, stdout/print)
+//    ->
+// 1. we're running this directly as a program, so i need a main()
+// 2. I need to handle CLI user inputs & outputs, so i need the dart:io module
+// 3. I need some sort of function that returns a List<int> (or similar series type) of numbers
+// 4. Somehow, I need to make sure that doesn't have duplicates
