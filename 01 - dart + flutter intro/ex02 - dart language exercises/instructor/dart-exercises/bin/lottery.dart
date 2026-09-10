f46fd812ed42ef508c9fe498235828f17b02047c
Oliver Antoniu <oliver+github@abolish.golf>
2026-09-10 11:03:31 -0600

    rev [01 - intro]: ex02 - ARRAYS: collect user inputs

--- 01 - dart + flutter intro/ex02 - dart language exercises/instructor/dart-exercises/bin/lottery.dart
+++ 01 - dart + flutter intro/ex02 - dart language exercises/instructor/dart-exercises/bin/lottery.dart
@@ -5,3 +5,14 @@
 // 2. I need to handle CLI user inputs & outputs, so i need the dart:io module
 // 3. I need some sort of function that returns a List<int> (or similar series type) of numbers
 // 4. Somehow, I need to make sure that doesn't have duplicates
+
+import 'dart:io'; // 2.
+
+void main() {     // 1.
+  // print is equivalent to stdout.writeln and also doesn't require an import of dart:io
+  print("Please enter how many lotto numbers are in the series.");
+  int count = int.parse(stdin.readLineSync()!); // ! tells compiler, "trust me bro; this won't be null" — not best practice, but for brevity here
+
+  print("What is the largest lotto number (inclusively)?");
+  int largestNum = int.parse(stdin.readLineSync()!);
+}
